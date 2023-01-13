6b39833325f9b1969ea41b5aa9ebee5f3359245e
Gabriel SACCO <gasacco>
2023-01-13 12:02:06 +0100

    Timer pas encore fonctionnel

--- software/app/main.c
+++ software/app/main.c
@@ -1,6 +1,9 @@
 #include <system.h>
 #include "altera_avalon_pio_regs.h"
-int main()
+#include "altera_avalon_timer_regs.h"
+#include "sys/alt_irq.h"
+
+/*int main()
 {
 
 	while(1)
@@ -19,5 +22,33 @@ int main()
 		}	
 
 	}
+	return 0;
+}
+*/
+
+
+unsigned int i =0;
+int cnt;
+static void timer_irq()
+{
+
+	if ((i&0xF) ==10)
+	{
+		i =i+6;
+	}
+	else if ((i&0xF0)>>4 ==10)
+	{
+		i=i+96;
+	}
+	IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,i);		
+	i++;
+	IOWR_ALTERA_AVALON_TIMER_STATUS(TIMER_0_BASE,0);
+}
+
+int main()
+{
+
+	alt_irq_register(TIMER_0_IRQ,NULL,timer_irq);
+	while(1);
 	return 0;
 }
